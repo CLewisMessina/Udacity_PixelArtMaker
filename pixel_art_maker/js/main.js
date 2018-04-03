c933ec6e8513eb29f34293c5206f6f799c991469
C Lewis Messina <33812538+CLewisMessina@users.noreply.github.com>
2018-04-02 22:55:02 -0500

    Add Files Via Upload

---
+++ pixel_art_maker/js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+// Select color input
+var height, width, color;
+
+// Select size input
+$('#sizePicker').submit(function(event) {
+  event.preventDefault();
+  height = $('#inputHeight').val();
+  width = $('#inputWidth').val();
+  makeGrid(height, width);
+  // console.log(height + width);
+
+});
+
+
+// Remove Grid When Reset Button Clicked
+ $( ".img-top2" ).on( "click", function(event) {
+   event.preventDefault();
+   makeGrid(0,0);
+ });
+
+
+// When size is submitted by the user, call makeGrid()
+function makeGrid(gridWidth, gridHeight) {
+  $('tr').remove();
+
+  for (var i = 1; i <= gridWidth; i++) {
+    $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+    for (var j = 1; j <= gridHeight; j++) {
+      $('#table' + i).append('<td></td>');
+    }
+  }
+
+
+  //add color to cell when selected by mouse
+  $('td').click(function addColor(){
+    color = $('#colorPicker').val();
+
+    if ($(this).attr('style')){
+      $(this).removeAttr('style')
+    } else {
+      $(this).attr('style', 'background-color:' + color);
+    }
+  })
+
+}
+});
